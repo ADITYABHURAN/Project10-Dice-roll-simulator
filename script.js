acd87432c70844be98d40ad714d1eb1e0652f6ac
Aditya Bhuran <adityabhuran240802@gmail.com>
2025-07-13 07:38:39 +0530

    added comments

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const resultMessage = document.getElementById('result');
 
 
 rollButton.addEventListener('click', rollDice);
-
+//for every face there is a unicode used 
 const diceFaces = ["\u2685", "\u2686", "\u2687", "\u2688", "\u2689", "\u268A"]; // Unicode characters for dice faces 1-6
 
 function rollDice() {
-    const randomIndex = Math.floor(Math.random() * 6);
+    //math.random generates a random number between 0 and 1, multiplying by 6 gives a range of 0 to 5, and Math.floor rounds it down to an integer between 0 and 5.
+    //Adding 1 to the result gives a range of 1 to 6, which corresponds to the dice faces.
+    const randomIndex = Math.floor(Math.random() * 6); 
     dice.textContent = diceFaces[randomIndex];
     resultMessage.textContent = `You rolled a ${randomIndex + 1}!`;
 }
